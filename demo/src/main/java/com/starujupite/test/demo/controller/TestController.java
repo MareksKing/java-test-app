52678bbf8210085a397e2a7548a7b9425d78a526
Mareks <robaldsmareks@gmail.com>
2024-04-22 23:47:35 +0300

    initial commit

---
+++ demo/src/main/java/com/starujupite/test/demo/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.starujupite.test.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@RequestMapping("/alive")
+@Slf4j
+public class TestController {
+    
+    @GetMapping
+    public ResponseEntity<String> getOKResponse() {
+        log.info("Accessed /alive endpoint");
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
+
+}
