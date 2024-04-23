bd0853b00e7664ace34b5bd1af2ff10d7ee5bcb2
r1v <emilssefl@gmail.com>
2024-04-23 09:10:43 +0300

    Added base GKE logging config

--- demo/src/main/java/com/starujupite/test/demo/DemoApplication.java
+++ demo/src/main/java/com/starujupite/test/demo/DemoApplication.java
@@ -1,19 +1,18 @@
 package com.starujupite.test.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
-
-	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
-
 	public static void main(String[] args) {
-		logger.info("Logging INFO with Logback");
-		logger.info("Running on server port: 80");
-		logger.error("Logging ERROR with Logback");
+		log.info("Logging INFO with Logback");
+		log.info("Running on server port: 80");
+		log.error("Logging ERROR with Logback");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
